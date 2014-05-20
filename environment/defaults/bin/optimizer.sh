4789eded635bf8e987a172e4ce05922842a5e180
jpoepjes <jpoepjes@DARB-HAN-APP1.ronin.roncap.com>
2014-05-20 08:52:30 +0100

    removed echo from optimizer.sh script

--- environment/defaults/bin/optimizer.sh
+++ environment/defaults/bin/optimizer.sh
@@ -3,5 +3,4 @@
 baseDir=/mnt/data/deploy/Release/lib
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib:/usr/local/lib64:$baseDir
-echo LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 ${baseDir}/ronin.optimizer $@
