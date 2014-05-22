b08e86d7bebe956e3e1bd70e28436acdae69eca6
Cristian Pirnog <pirnog@gmail.com>
2014-05-22 10:22:33 +0200

    Summary: improvements. Details: 	1. environment/defaults/bin/stash: added check that there is a stash id before doing stash operations. 	2. environment/defaults/completions/optimizer.sh: added ronin.optimizer.

--- environment/defaults/completions/optimizer.sh
+++ environment/defaults/completions/optimizer.sh
@@ -2,7 +2,9 @@ _optimizer()
 {
   cur=${COMP_WORDS[COMP_CWORD]}
   #COMPREPLY=( $( compgen -W "$(./rocardian.optimizer listCommands})" -- $cur ) )
-  COMPREPLY=( $( compgen -W "correlationAnalysis printData DataQuality expiryAnalysis LiquidityAnalysis optimize OrderLogParser simulate staticInfo " -- $cur ) )
+  COMPREPLY=( $( compgen -W "correlationAnalysis printData DataQuality expiryAnalysis LiquidityAnalysis optimize OrderLogParser simulate staticInfo translate" -- $cur ) )
 }
 
 complete -o default -o filenames -F _optimizer ./rocardian.optimizer
+complete -o default -o filenames -F _optimizer ./ronin.optimizer
+
