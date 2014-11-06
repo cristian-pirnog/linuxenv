42ec78167d4e7a17d15123cea2778a78238da48f
ArByte <arbytetest@parb-aur-app1.ronin.roncap.com>
2014-11-06 14:35:54 +0000

    Summary: Details: 	1. .gitignore: added rule for vi swap files. 	2. environment/xcustoms/arbyte.prod/CRON/base.sh: development. 	3. environment/xcustoms/arbyte.prod/CRON/shutdown.sh: same as point 2. 	4. Added file environment/xcustoms/arbyte.test/.aliases_custom. 	5. environment/xcustoms/arbyte.test/CRON/cron.tab: disabled cron jobs for the user. 	6. environment/xcustoms/arbyte.test/config.install.arbyte: updated according to point 7. 	7. Added file environment/xcustoms/arbyte.test/userenv_arbyte.

--- environment/xcustoms/arbyte.prod/CRON/base.sh
+++ environment/xcustoms/arbyte.prod/CRON/base.sh
@@ -12,6 +12,15 @@ datDir=${liveDir}/dat
 
 cmeRecoverFile=${datDir}/cmearbp1_recover.csv
 
+complianceLogDir=/mnt/prodlog.arbyte.share
+if [[ $USER == "arbytetest" ]]; then
+	complianceLogDir=${complianceLogDir}/test
+fi
+
+arbyteNAS=livebkp@10.199.10.104
+arbyteLiveBkpBaseDir=/volume1/live.logs
+arbyteLiveBkpDir=${arbyteNAS}:${arbyteLiveBkpBaseDir}
+
 date=$(date +%Y%m%d)
 
 
