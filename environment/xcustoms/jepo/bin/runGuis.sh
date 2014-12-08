257df9872ac5d8b1acff0982239db6aaf7f7cc8a
jepo <poepjens@hotmail.com>
2014-12-08 09:45:02 +0100

    Debugging new script for starting VPlatGUIs

--- environment/xcustoms/jepo/bin/runGuis.sh
+++ environment/xcustoms/jepo/bin/runGuis.sh
@@ -1,6 +1,6 @@
 cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/cme
-./rungui &>/dev/null
+./rungui &>/dev/null &
 cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/eux
-./rungui &>/dev/null
+./rungui &>/dev/null &
 #cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/lif
-#./rungui &>/dev/null
+#./rungui &>/dev/null &
