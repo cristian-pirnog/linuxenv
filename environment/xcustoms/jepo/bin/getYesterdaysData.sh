f3b1ec95cbae487dace7b9da86ab47da95b30f70
Jens Poepjes <jens.poepjes@gmail.com>
2014-12-12 15:04:11 +0000

    environment/xcustoms/jepo/config.install.jpoepjes: added such that new scripts are automatically put in /home/jpoepjes/bin when running update_user, environment/xcustoms/jepo/bin/getYesterdaysData.sh: fine tuned, debugged, added some features, e.g. not running when all data is there, environment/xcustoms/jepo/CRON/cron.tab.DARB-HAN-APP1: debugged and now final crontab

--- environment/xcustoms/jepo/bin/getYesterdaysData.sh
+++ environment/xcustoms/jepo/bin/getYesterdaysData.sh
@@ -20,20 +20,37 @@ then
         echo "$outputDir NOT FOUND!"
 fi
 
+YYYYMMDD=$(date +"%Y%m%d")
+
+didRunForDay=$(ls $outputDir | grep missingData_$YYYYMMDD | wc -l)
+#echo "didRunForDay = $didRunForDay"
+if [ $didRunForDay -ge 1 ]
+then
+	latestMissingDataFile=$(ls $outputDir | grep missingData_$YYYYMMDD | tail -1)
+	isDataMissing=$(wc -l $latestMissingDataFile | awk '{print $1}')
+	#echo "latestMissingDataFile= $latestMissingDataFile , isDataMissing= $isDataMissing"
+	if [ $isDataMissing -eq 0 ]
+	then
+		echo "All data for $YYYYMMDD is there; finished"
+		exit 1
+	fi
+fi
+
 # create file with yesterdays date
 datesFile=$outputDir/date.txt
-date --date="1 days ago" +"%Y%m%d" > $outputDir/$datesFile
+date --date="1 days ago" +"%Y%m%d" > $datesFile
 
-#createMBBAdata.sh $prodFile $outputDir/$datesFile $outputDir &>/dev/null
-rm -f $outputDir/$datesFile
+createMBBAdata.sh $prodFile $datesFile $outputDir &>/dev/null
+rm -f $datesFile
+
+# remove old missingData_YYYYMMDD_hhmm.csv files
+oldFilesNames="missingData_"$YYYYMMDD"_*.csv"
+rm -f $outputDir/$oldFilesNames
 
 # Rename missingData.csv file to missingData_YYYYMMDD_hhmm.csv
 YYYYMMDD_hhmm=$(date +"%Y%m%d_%H%M")
 newFileName="missingData_"$YYYYMMDD_hhmm".csv"
 #echo $newFileName
-
-touch $newFileName
-exit 1
 
 if [ ! -f $outputDir/missingData.csv ]
 then
