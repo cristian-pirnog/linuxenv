f49ed72ec30f3868c7eb25ed11dde8702f6b5ca1
Production User <pirnog@gmail.com>
2015-01-18 15:56:27 +0100

    Summary: Details: 	1. environment/defaults/.bash_profile: minor change. 	2. environment/xcustoms/production/bin/keepUpToDate.sh: added stuff for logging.

--- environment/xcustoms/production/bin/keepUpToDate.sh
+++ environment/xcustoms/production/bin/keepUpToDate.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+source ~/.bash_profile
+mkdir -p ~/log
+logFile="${HOME}/log/keepUpToDate.$(date +%Y%m%d-%H%M%S).log"
 
 # The code
 cd ~/code 
-pula
-buildall ron Release install
+pula >> ${logFile} 2>&1
+buildall ron Release install >> ${logFile} 2>&1
 
 # The production stuff
 cd ~/production
-pula
+pula >> ${logFile} 2>&1
 
