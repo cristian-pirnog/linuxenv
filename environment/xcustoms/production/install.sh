ab3617209bd1503c9f44009e3d4daa665ffe2d67
Production User <pirnog@gmail.com>
2015-02-01 22:44:04 +0100

    Summary: improvements. Details: 	1. environment/xcustoms/install.sh: corrected spelling error. 	2. environment/xcustoms/production/CRON/cron.tab.baar: automatic update. 	3. environment/xcustoms/production/CRON/cron.tab.luzern: same as point 2. 	4. Added file environment/xcustoms/production/CRON/cron.tab.template. 	5. environment/xcustoms/production/CRON/cron.tab.zurich: same as point 2. 	6. Added file environment/xcustoms/production/install.sh.

---
+++ environment/xcustoms/production/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cron run times: script name HH MM
+cronRunTimes='createBookConfigs.sh 22 30
+deploySoftware 22 40'
+
+# The list of hosts where the user is deployed
+hosts='baar
+luzern
+zurich'
+
+for h in $hosts; do
+    cp CRON/cron.tab.template CRON/cron.tab.${h}
+done
+
+while read -r line; do
+    script=$(echo ${line} | awk '{print $1}')
+    hour=$(echo ${line} | awk '{print $2}')
+    minute=$(echo ${line} | awk '{print $3}')
+
+    offset=0
+    for h in $hosts; do
+        time=$(date +'%M %H' --date="${hour}:${minute} ${offset} minutes")
+        sed -i "/${script}/s/__MM_HH__/${time}/" CRON/cron.tab.${h}
+
+        offset=$((offset + 10))
+    done
+done <<< "${cronRunTimes}"
