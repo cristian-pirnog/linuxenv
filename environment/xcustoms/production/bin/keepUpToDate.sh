fb3fea43ade6871ad3ec90fc6de1c298069ae7e0
Production User <pirnog@gmail.com>
2015-04-23 11:58:40 +0200

    Summary: minor improvements. Details: 	1. environment/xcustoms/production/bin/keepUpToDate.sh: better logging; added section to keep the user config up to date.

--- environment/xcustoms/production/bin/keepUpToDate.sh
+++ environment/xcustoms/production/bin/keepUpToDate.sh
@@ -10,13 +10,18 @@ find ${logDir} -type f -mtime +3 -exec rm {} \;
 
 # The code
 cd ~/code 
+pwd >> ${logFile} 2>&1
+branch -s master >> ${logFile} 2>&1
 pula >> ${logFile} 2>&1
 buildall ron Release install >> ${logFile} 2>&1
 
 # The production stuff
 cd ~/production
+pwd >> ${logFile} 2>&1
 pula >> ${logFile} 2>&1
 
-
-
+# The user config stuff
+cd ~/.${USER}_config
+pwd >> ${logFile} 2>&1
+pula >> ${logFile} 2>&1
 
