45b3cb3b6194bc1b7ce62fe869ab7f207fe92afc
Cristian Pirnog <pirnog@gmail.com>
2015-08-13 23:08:25 +0200

    Summary: minor change. Details: 	1. environment/defaults/completions/bashScript.sh: removed some echo statements.

--- environment/defaults/completions/bashScript.sh
+++ environment/defaults/completions/bashScript.sh
@@ -5,12 +5,10 @@ _bashScript()
 }
 
 scriptDirs="${HOME}/CRON ${HOME}/bin"
-grep -l -- --listOptions $(awk '{for(i=1;i<=NF;i++) print $i"/*"}' <<< ${scriptDirs})
 supportedScripts=$(grep -l -- --listOptions $(awk '{for(i=1;i<=NF;i++) print $i"/*"}' <<< ${scriptDirs}))
 
 echo supportedScripts=${supportedScripts}
 for s in ${supportedScripts}; do
-  echo $s $(basename ${s}) './'$(basename ${s}) $(sed "s_${HOME}_~_" <<< ${s})
   complete -o default -o filenames -F _bashScript ${s} $(basename ${s}) ./$(basename ${s}) $(sed "s_${HOME}_~_" <<< ${s})
 done
 
