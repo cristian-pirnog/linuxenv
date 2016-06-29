c28dff034060abfa1abaca72197679839975ebea
Cristian Pirnog <cristian.pirnog@devicetools.ch>
2016-06-29 15:15:01 +0200

    removed unused users

--- environment/xcustoms/jepo/bin/runGuis.sh
+++
@@ -1,6 +0,0 @@
-cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/cme
-./rungui &>/dev/null &
-cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/eux
-./rungui &>/dev/null &
-cd /home/jepo/code/ronin/libs/monitoring/VplatGUI/liffe
-./rungui &>/dev/null &
