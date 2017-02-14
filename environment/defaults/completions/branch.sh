bfe61a44af2f39fafdfcaa82e2777f213af5948d
Cristian Pirnog <pirnog@gmail.com>
2017-02-14 15:08:19 +0100

    Summary: improvements in environment. Details: 	1. environment/defaults/bin/br: changed to put each option on a new line (necessary for compgen). 	2. environment/defaults/completions/branch.sh: changed to remove spaces in front of branch names in compgen arguments. 	3. environment/xcustoms/crpi.base/atom/config.cson: atom config stuff. 	4. environment/xcustoms/crpi.base/atom/keymap.cson: same as point 3. 	5. environment/xcustoms/crpi.work/.aliases_custom: updates. 	6. environment/xcustoms/crpi.work/.userenv_custom: same as point 5.

--- environment/defaults/completions/branch.sh
+++ environment/defaults/completions/branch.sh
@@ -1,7 +1,7 @@
 _branch()
 {
   cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $( compgen -W "$(br --listOptions) $(br -lall | replace '*' ' ' | grep '^ ' | sort | uniq)" -- $cur ) )
+  COMPREPLY=( $( compgen -W "$(br --listOptions) $(br -lall | replace '*' ' ' | grep '^ ' | sort | uniq | sed 's/ //g')" -- $cur ) )
 }
 
-complete -o default -o filenames -F _branch br
+complete -o filenames -F _branch br
