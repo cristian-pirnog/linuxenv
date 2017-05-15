1bb21a95bebd6be57631772258657ae27e66c561
Cristian Pirnog <pirnog@gmail.com>
2017-05-15 13:38:59 +0200

    added back completion script for br

---
+++ environment/defaults/completions/br.sh
@@ -0,0 +1,10 @@
+_branch()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+    # The list of branches comes on the next line, otherwise the last option and the
+    # first branch will be considered as one unit
+  COMPREPLY=( $( compgen -W "$(br --listOptions)
+$(br -lall | sed 's/\*//g' | grep '^ ' | sort | uniq | sed 's/ //g')" -- $cur ) )
+}
+
+complete -o filenames -F _branch br
