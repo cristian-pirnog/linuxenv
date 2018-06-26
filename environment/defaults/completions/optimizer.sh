1d83c2c554e32353e4e7d344008b1ca6e4f6bd63
Cristian Pirnog <pirnog@gmail.com>
2018-06-26 09:47:39 +0200

    Summary: improvements. Details: 	1. Deleted file environment/defaults/completions/optimizer.sh. 	2. Deleted file environment/defaults/completions/stack.sh. 	3. environment/xcustoms/crpi.base/bin/makeVersion: made code for handling supported build dirs generic.

--- environment/defaults/completions/optimizer.sh
+++
@@ -1,9 +0,0 @@
-_optimizer()
-{
-  cur=${COMP_WORDS[COMP_CWORD]}
-  #COMPREPLY=( $( compgen -W "$(optimizer listCommands})" -- $cur ) )
-  COMPREPLY=( $( compgen -W "correlationAnalysis edit printData DataQuality expiryAnalysis LiquidityAnalysis optimize OrderLogParser simulate staticInfo translate" -- $cur ) )
-}
-
-complete -o default -o filenames -F _optimizer optimizer
-
--- environment/defaults/completions/stack.sh
+++
@@ -1,3 +0,0 @@
-if [[ -n $(which stack 2>/dev/null) ]]; then
-  eval "$(stack --bash-completion-script stack)"
-fi
